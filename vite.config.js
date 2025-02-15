b855912a89907d901cb2fdc9cdf6071fe68655b8
ninaxle <166761228+ninaxle@users.noreply.github.com>
2025-02-15 14:58:24 -0500

    Added first files
    File set-up

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: resolve(__dirname, "src"), // Make sure this is an absolute path
+  build: {
+    emptyOutDir: true, // Add this to clear the output directory
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
+  publicDir: resolve(__dirname, "assets"), // Use absolute path for public directory
+});
