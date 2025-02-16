9b1f2c5c5589d11c3425397f24d5e45c1cceaf9e
ninaxle <166761228+ninaxle@users.noreply.github.com>
2025-02-15 20:58:04 -0500

    first page done

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   root: resolve(__dirname, "src"), // Make sure this is an absolute path
@@ -15,4 +16,7 @@ export default defineConfig({
     port: 5173,
   },
   publicDir: resolve(__dirname, "assets"), // Use absolute path for public directory
+  plugins: [
+    tailwindcss(),
+  ],
 });
